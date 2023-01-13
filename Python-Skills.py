e834f142d5aa049db21fab3a88a5895d15710cab
Blake857 <Blakehgoss@gmail.com>
2023-01-13 15:11:15 +1100

    Added highest function
    A simple function to determine the highest of 3 numbers

--- Python-Skills.py
+++ Python-Skills.py
@@ -2,10 +2,18 @@ def square(n):
     n = n*n
     return n
 
+def highest(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b > a and b >= c:
+        return b
+    else:
+        return c
 
 
 def main():
     print(square(20))
+    print(highest(5, 4, 10))
 
 if __name__ == "__main__":
     main()
