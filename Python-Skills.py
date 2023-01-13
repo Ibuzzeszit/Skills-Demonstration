71f23e3cbaba4b683859951c29cc337027f09538
Blake857 <Blakehgoss@gmail.com>
2023-01-13 17:06:42 +1100

    Added more functions
    Added two new functions and improved the format for the output

--- Python-Skills.py
+++ Python-Skills.py
@@ -10,10 +10,34 @@ def highest(a, b, c):
     else:
         return c
 
+def charInString(char, string):
+    for x in string:
+        if x == char:
+            return True
+    return False
+
+def multiple(list):
+    count = []
+    for x in list:
+        for y in count:
+            if x == y:
+                return y
+        count.append(x)
+    return "No numbers appear multiple times"
+
 
 def main():
-    print(square(20))
-    print(highest(5, 4, 10))
+    print("Square: %s" % square(20))
+    print("")
+
+    print("Highest number: %s" % highest(5, 4, 10))
+    print("")
+
+    print("The character is in the String: %s" % charInString('s', "faster"))
+    print("")
+
+    print("Number that appears multiple times: %s " % multiple([12, 8, 55, 23, 12]))
+    print("")
 
 if __name__ == "__main__":
     main()
